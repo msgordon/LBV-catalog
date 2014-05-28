92bf66692e1537863f2df9b402b93099b02b4baa
Michael Gordon <msgordon.astro@gmail.com>
2014-05-28 09:44:23 -0500

    Update log plot

--- LBV_SEDplot.py
+++ LBV_SEDplot.py
@@ -16,28 +16,28 @@ bb2= lambda wave,A,B,C,D: bb(wave,A,B) + bb(wave,C,D)
 def plot_SED(tableFile):
     table = Table.read(tableFile)
 
-    pp = PdfPages('BB.pdf')
+    pp = PdfPages('BB2.pdf')
     
     waves = np.array([float(x[6:10]) for x in table.columns[1:]])
     for star in table:
         phot = [x if x != 99.99 else np.NaN for x in star.data][1:]
 
         try:
-            popt, pcov = curve_fit(bb,waves,phot,p0=(phot[0],1e-2))
-            #popt, pcov = curve_fit(bb2,waves,phot,p0=(phot[0],1e-2,phot[4],1e-3))
+            #popt, pcov = curve_fit(bb,waves,phot,p0=(phot[0],1e-2))
+            popt, pcov = curve_fit(bb2,waves,phot,p0=(phot[0],1e-2,phot[4],1e-3))
         except:
             continue
 
         print 'Fitting %s' % star['ID']
         T = (h*c/k)/popt[1]*10000.
-        #fit = bb2(waves,*popt)
-        fit = bb(waves,*popt)
+        fit = bb2(waves,*popt)
+        #fit = bb(waves,*popt)
         fig = plt.figure()
-        plt.plot(waves,phot,'ro')
-        plt.plot(waves,fit,'b-')
+        plt.plot(waves,np.log10(phot),'ro')
+        plt.plot(waves,np.log10(fit),'b-')
         #plt.plot(waves,bb(waves,1e-7,7000)/np.max(bb(waves,1e-7,7000)),linewidth=2)
         plt.xlabel(r'$\lambda\,[\mu m]$')
-        plt.ylabel(r'$\lambda\,F_{\lambda}\,[erg\,sec^{-1}\,cm^{-2}]$')
+        plt.ylabel(r'$log[\lambda\,F_{\lambda}]\,[erg\,sec^{-1}\,cm^{-2}]$')
         plt.title(star['ID'])
         plt.legend(['Photometry','BB(T = %f)'%T])
 
