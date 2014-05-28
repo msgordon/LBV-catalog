a4f2de3234bc5f0be23f5d6b5227263f21faff36
Michael Gordon <msgordon.astro@gmail.com>
2014-05-27 20:16:04 -0500

    BB fitting

--- LBV_SEDplot.py
+++ LBV_SEDplot.py
@@ -1,20 +1,53 @@
 #! /usr/bin/env python
 from astropy.table import Table
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
 import argparse
+import numpy as np
+from scipy.optimize import curve_fit
+
+h = 4.135668e-15  # eV*s
+c = 3.0e10        # cm/s
+k = 8.617e-5      # eV/K
+
+bb = lambda wave,A,B: A/np.power(wave,5) * 1.0/(np.exp(B/wave)-1.0)
+bb2= lambda wave,A,B,C,D: bb(wave,A,B) + bb(wave,C,D)
 
 def plot_SED(tableFile):
     table = Table.read(tableFile)
 
-    waves = [float(x[6:10]) for x in table.columns[1:]]
+    pp = PdfPages('BB.pdf')
+    
+    waves = np.array([float(x[6:10]) for x in table.columns[1:]])
     for star in table:
-        phot = [x for x in star.data][1:]
-        plt.plot(waves,phot,'ro')
+        phot = [x if x != 99.99 else np.NaN for x in star.data][1:]
 
+        try:
+            popt, pcov = curve_fit(bb,waves,phot,p0=(phot[0],1e-2))
+            #popt, pcov = curve_fit(bb2,waves,phot,p0=(phot[0],1e-2,phot[4],1e-3))
+        except:
+            continue
+
+        print 'Fitting %s' % star['ID']
+        T = (h*c/k)/popt[1]*10000.
+        #fit = bb2(waves,*popt)
+        fit = bb(waves,*popt)
+        fig = plt.figure()
+        plt.plot(waves,phot,'ro')
+        plt.plot(waves,fit,'b-')
+        #plt.plot(waves,bb(waves,1e-7,7000)/np.max(bb(waves,1e-7,7000)),linewidth=2)
         plt.xlabel(r'$\lambda\,[\mu m]$')
         plt.ylabel(r'$\lambda\,F_{\lambda}\,[erg\,sec^{-1}\,cm^{-2}]$')
-        plt.show()
-        exit()
+        plt.title(star['ID'])
+        plt.legend(['Photometry','BB(T = %f)'%T])
+
+        pp.savefig(fig)
+        #plt.show()
+        #exit()
+
+    pp.close()
+    #plt.show()
+    exit()
         
 
 
