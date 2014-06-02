775dc68f61d2f3bd1739d4e39f27effee2cfcba5
Michael Gordon <msgordon.astro@gmail.com>
2014-06-02 12:17:02 -0400

    Diddled with fitting

--- LBV_SEDplot.py
+++ LBV_SEDplot.py
@@ -6,29 +6,83 @@ import argparse
 import numpy as np
 from scipy.optimize import curve_fit
 
-h = 4.135668e-15  # eV*s
-c = 3.0e10        # cm/s
-k = 8.617e-5      # eV/K
+h = 6.626e-27  # erg*s
+c = 3.0e10     # cm/s
+k = 1.381e-16  # erg/K
+
+
+bbtemp = lambda wave,T: 2*h*c**2/np.power(wave,5) * 1.0/(np.exp(h*c/(k*wave*T)) - 1.0)
 
 bb = lambda wave,A,B: A/np.power(wave,5) * 1.0/(np.exp(B/wave)-1.0)
+bblog = lambda wave,A,B: A - 5.0*np.log10(wave) - np.log10(np.exp(B/wave)-1.0)
+
 bb2= lambda wave,A,B,C,D: bb(wave,A,B) + bb(wave,C,D)
+bblog2 = lambda wave,A,B,C,D: np.log10(10.0**bblog(wave,A,B) + 10.0**bblog(wave,C,D))
 
 def plot_SED(tableFile):
     table = Table.read(tableFile)
 
-    pp = PdfPages('BB2.pdf')
+    waves = np.array([float(x[6:10]) for x in table.columns[1:]])[:5]
+    #print waves
+    #exit()
+    wavesCM = waves*1e-4
+
+    wavesDISP = np.linspace(waves[0],waves[-1],num=100)
+    #pp = PdfPages('BB2.pdf')
     
-    waves = np.array([float(x[6:10]) for x in table.columns[1:]])
     for star in table:
         phot = [x if x != 99.99 else np.NaN for x in star.data][1:]
+        phot = phot[:5]
+        popt,pcov = curve_fit(bbtemp,wavesCM,phot,p0=14000)
+        
+        #popt,pcov = curve_fit(bbtemp,wavesCM[0:2],phot[0:2],p0=14000)
 
+        print popt
+        fit = bbtemp(waves,*popt)
+        
+        plt.plot(waves,np.log10(phot),'ro')
+        plt.plot(waves,np.log10(fit),'b-')
+        plt.show()
+        exit()
+
+
+    
+
+
+        '''
         try:
             #popt, pcov = curve_fit(bb,waves,phot,p0=(phot[0],1e-2))
-            popt, pcov = curve_fit(bb2,waves,phot,p0=(phot[0],1e-2,phot[4],1e-3))
+            #popt, pcov = curve_fit(bb2,waves,phot,p0=(phot[0],1e-2,phot[4],1e-3))
+            #popt, pcov = curve_fit(bblog2,waves,np.log10(phot),p0=(np.log10(phot[0]),-2.0,np.log10(phot[4]),-3.0))
+            idx1 = range(0,5)
+            idx2 = range(5,len(waves))
+            popt1,pcov1 = curve_fit(bb,waves[idx1],phot[idx1])
+            popt2,pcov2 = curve_fit(bb,waves[idx2],phot[idx2])
+            
         except:
             continue
-
+        '''
         print 'Fitting %s' % star['ID']
+
+        popt1,pcov1 = curve_fit(bb,waves[0:5],phot[0:5])
+        popt2,pcov2 = curve_fit(bb,waves[7:],phot[7:])
+        #
+        print popt1,popt2
+        #fit = bblog2(waves,*popt)
+        fit1 = bb(waves,*popt1)
+        fit2 = bb(waves,*popt2)
+        fig = plt.figure()
+        plt.plot(waves,np.log10(phot),'ro')
+        plt.plot(waves,np.log10(fit1+fit2),'b--',linewidth=2)
+        plt.plot(waves,np.log10(fit1),'b-',linewidth=2)
+        plt.plot(waves,np.log10(fit2),'m-',linewidth=2)
+        #plt.plot(waves,np.log10(bb(waves,popt[0],popt[1])),'b-',linewidth=2)
+        #plt.plot(waves,np.log10(bb(waves,popt[2],popt[3])),'b-',linewidth=2)
+        plt.show()
+        exit()
+        #
+
+        
         T = (h*c/k)/popt[1]*10000.
         fit = bb2(waves,*popt)
         #fit = bb(waves,*popt)
@@ -45,7 +99,7 @@ def plot_SED(tableFile):
         #plt.show()
         #exit()
 
-    pp.close()
+    #pp.close()
     #plt.show()
     exit()
         
